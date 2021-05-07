366925aea7ffb17da85be6537513a4d879ee4fe7
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-07 11:19:52 +0000

    primer get hecho

--- src/main.py
+++ src/main.py
@@ -31,13 +31,10 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+def get_users():
+    users = user.query.all()
+    serialized_users = list(map(lambda user:user.serialize(),users))
+    return jsonify(serialized_users), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
