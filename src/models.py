11ace2c2d1d8734f33ac1f402c2b2c2a0d50efc8
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-07 11:25:09 +0000

    primers tablas de la API hechas, revisarlas

--- src/models.py
+++ src/models.py
@@ -17,3 +17,45 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+# class Home (Base):
+#      __tablename__ = 'home'
+#      id = Column(Integer, primary_key=True)
+#      password = Column(Integer, ForeignKey('Login.Password'))
+#      user_name =  Column(Integer, ForeignKey('User.user_name'))
+#      url = Column(String(250), nullable=False)
+#      user_id = Column(Integer, ForeignKey('user.id'))
+#      post = Column(Integer, ForeignKey('post.id'))
+#      planets_post = Column(Integer, ForeignKey('planets.post'))
+#      characters_post = Column(Integer, ForeignKey('characters.post'))
+     
+
+# class User(Base):
+#      __tablename__ = 'User'
+#      id = Column(Integer, primary_key=True)
+#      username = Column(String(250))
+#      password = Column(Integer, ForeignKey('Login.password'))
+#      email = Column(String(250), nullable=False)
+#      post = Column(Integer, ForeignKey('post.id'))
+
+# class Like (Base):
+#      __tablename__ = 'like'
+#      # Here we define columns for the table address.
+#      # Notice that each column is also a normal Python instance attribute.
+#      id = Column(Integer, primary_key=True)
+#      likes = Column(String(250))
+#      user_id = Column(Integer, ForeignKey('post.id'))
+#      like = Column(Integer, ForeignKey('like.id'))
+#      planets_post = Column(Integer, ForeignKey ('planets.post'))
+#      characters_post = Column(Integer, ForeignKey('characters.post'))
+     
+# class Characters (Base):
+#      __tablename__ = 'characters'
+#      # Here we define columns for the table address.
+#      # Notice that each column is also a normal Python instance attribute.
+#      id = Column(Integer, primary_key=True)
+#      likes = Column(String(250))
+#      user_id = Column(Integer, ForeignKey('user.id'))
+#      like = Column(Integer, ForeignKey('like.id'))
+#      user = relationship ('User')    
